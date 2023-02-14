44f0c1d4aeb95a3dc08c82046f1df04838de2ff8
AzTejas <tejaspatel191@gmail.com>
2023-02-14 21:58:28 +0000

    first commit

---
+++ java/JavaTopics/JavaTopics.java
@@ -0,0 +1,16 @@
+package JavaTopics;
+/*Changed the name from
+Java to Javatopics
+ */
+
+public class JavaTopics {
+    public static void main(String[] args) {
+        System.out.println("These are the Topics of Java");
+        System.out.println("Uses of Java");
+        System.out.println("Java Features");
+        System.out.println("IDE");
+        System.out.println("Packages");
+        System.out.println("Import");
+        System.out.println("Class");
+    }
+}
---
+++ java/ManualTopics/ManualTopics.java
@@ -0,0 +1,19 @@
+package ManualTopics;
+// changed the package name from Manual to ManualTopics
+
+public class ManualTopics {
+    public static void main(String[] args) {
+        System.out.println("10 Manual Testing Topics:");
+        System.out.println("Test Plan");
+        System.out.println("STLC");
+        System.out.println("SDLC");
+        System.out.println("Test Cases");
+        System.out.println("Testing Techniques");
+        System.out.println("Types of Bugs");
+        System.out.println("Bug Life cycle");
+        System.out.println("Types of Testing tools");
+        System.out.println("Blackbox Testing");
+        System.out.println("Whitebox Testing");
+
+    }
+}
